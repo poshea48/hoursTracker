d8aa1542e497950a1ec43dd7f1dfef0571f45324
Paul O'Shea <poshea48@msn.com>
2019-04-02 08:19:32 -1000

    changed users schema id column to onDelete(CASCADE)

--- db/migrations/20190123112354_create_users_and_logged_work_and_archive_months_tables.js
+++ db/migrations/20190123112354_create_users_and_logged_work_and_archive_months_tables.js
@@ -1,31 +1,38 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', table => {
-      table.increments('id').primary()
-      table.string('name').notNullable();
-      table.string('email').unique().notNullable();
-      table.string('token')
-      table.string('password_digest')
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
+  return knex.schema
+    .createTable("users", table => {
+      table
+        .increments("id")
+        .primary()
+        .onDelete("CASCADE");
+      table.string("name").notNullable();
+      table
+        .string("email")
+        .unique()
+        .notNullable();
+      table.string("token");
+      table.string("password_digest");
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
     })
-    .createTable('logged_work', table => {
-      table.increments('id').primary()
-      table.integer('user_id').references('users.id')
-      table.float('hrs_worked')
-      table.date('log_day').defaultTo(knex.fn.now())
+    .createTable("logged_work", table => {
+      table.increments("id").primary();
+      table.integer("user_id").references("users.id");
+      table.float("hrs_worked");
+      table.date("log_day").defaultTo(knex.fn.now());
     })
-    .createTable('archive_months', table => {
+    .createTable("archive_months", table => {
       table.increments();
-      table.integer('user_id').references('users.id');
-      table.integer('month');
-      table.integer('year');
-      table.float('hrs_worked')
-    })
-}
+      table.integer("user_id").references("users.id");
+      table.integer("month");
+      table.integer("year");
+      table.float("hrs_worked");
+    });
+};
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('archive_months')
-    .dropTable('logged_work')
-    .dropTable('users')
-}
+  return knex.schema
+    .dropTable("archive_months")
+    .dropTable("logged_work")
+    .dropTable("users");
+};
