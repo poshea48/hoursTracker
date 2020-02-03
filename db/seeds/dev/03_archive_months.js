270ab05368809b33a404428eb5f2eb0cd65a138f
Paul O'Shea <poshea48@msn.com>
2020-02-03 12:41:46 -1000

    updated seed data

--- db/seeds/dev/03_archive_months.js
+++ db/seeds/dev/03_archive_months.js
@@ -4,12 +4,15 @@ exports.seed = function(knex, Promise) {
     .del()
     .then(function() {
       // Inserts seed entries
+      let sample_user = knex("users")
+        .where({ email: "sample@email.com" })
+        .select("id");
       return knex("archive_months").insert([
-        { user_id: 6, month: 7, year: 2019, hrs_worked: 128.869 },
-        { user_id: 6, month: 8, year: 2019, hrs_worked: 147.505 },
-        { user_id: 6, month: 9, year: 2019, hrs_worked: 116.564 },
-        { user_id: 6, month: 10, year: 2019, hrs_worked: 134.264 },
-        { user_id: 6, month: 11, year: 2019, hrs_worked: 169.04 }
+        { user_id: sample_user, month: 7, year: 2019, hrs_worked: 128.869 },
+        { user_id: sample_user, month: 8, year: 2019, hrs_worked: 147.505 },
+        { user_id: sample_user, month: 9, year: 2019, hrs_worked: 116.564 },
+        { user_id: sample_user, month: 10, year: 2019, hrs_worked: 134.264 },
+        { user_id: sample_user, month: 11, year: 2019, hrs_worked: 169.04 }
       ]);
     });
 };
--- db/seeds/dev/basecamp_hours.js
+++ db/seeds/dev/basecamp_hours.js
@@ -4,16 +4,64 @@ exports.seed = function(knex) {
     .del()
     .then(function() {
       // Inserts seed entries
+      let sample_user = knex("users")
+        .where({ email: "sample@email.com" })
+        .select("id");
       return knex("project_hours").insert([
-        { project_id: 3, user_id: 3, hrs_worked: 3.3, log_day: "2020-01-17" },
-        { project_id: 3, user_id: 3, hrs_worked: 4.2, log_day: "2020-01-21" },
-        { project_id: 3, user_id: 3, hrs_worked: 4.6, log_day: "2020-01-22" },
-        { project_id: 3, user_id: 3, hrs_worked: 3.8, log_day: "2020-01-27" },
-        { project_id: 3, user_id: 3, hrs_worked: 4.1, log_day: "2020-01-28" },
-        { project_id: 3, user_id: 3, hrs_worked: 5.8, log_day: "2020-01-30" },
-        { project_id: 3, user_id: 3, hrs_worked: 6.2, log_day: "2020-01-31" },
-        { project_id: 3, user_id: 3, hrs_worked: 2.3, log_day: "2020-02-01" },
-        { project_id: 3, user_id: 3, hrs_worked: 3.8, log_day: "2020-02-02" }
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 3.3,
+          log_day: "2020-01-17"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 4.2,
+          log_day: "2020-01-21"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 4.6,
+          log_day: "2020-01-22"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 3.8,
+          log_day: "2020-01-27"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 4.1,
+          log_day: "2020-01-28"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 5.8,
+          log_day: "2020-01-30"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 6.2,
+          log_day: "2020-01-31"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 2.3,
+          log_day: "2020-02-01"
+        },
+        {
+          project_id: 3,
+          user_id: sample_user,
+          hrs_worked: 3.8,
+          log_day: "2020-02-02"
+        }
       ]);
     });
 };
